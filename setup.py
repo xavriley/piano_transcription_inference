a21d2e2de54ca5c832933dddbb2004e33a1e65f2
Xavier Riley <xavriley@users.noreply.github.com>
2024-09-05 11:20:04 +0100

    Remove version requirement for librosa

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['matplotlib', 'mido', 'librosa==0.6.3', 'torchlibrosa==0.0.9'],
+    install_requires=['matplotlib', 'mido', 'librosa', 'torchlibrosa==0.0.9'],
     python_requires='>=3.6',
 )
